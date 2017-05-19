1b3937db2d64d097b2ca486934b84b33203c42f4
vvok12 <v.v.bezshtanko@gmail.com>
2017-05-19 13:38:01 +0300

    first hw completed

---
+++ Assets/Behaviour/HeroFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabitBehaviour hero;
+
+	// Update is called once per frame
+	void Update () {
+
+		Transform h_tr = hero.transform;
+		Transform this_tr = this.transform;
+
+		Vector3 h_pos = h_tr.position;
+		Vector3 this_pos = this_tr.position;
+
+		this_pos.x = h_pos.x;
+		this_pos.y = h_pos.y;
+
+		this_tr.position = this_pos;
+	}
+}
---
+++ Assets/Behaviour/HeroRabitBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabitBehaviour : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	private Rigidbody2D myRB = null;
+
+	// Use this for initialization
+	void Start () {
+		myRB = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+	//update is called once per frame
+	void FixedUpdate () {
+		float value = Input.GetAxis ("Horizontal");
+
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myRB.velocity;
+			vel.x = value * speed;
+			myRB.velocity = vel;
+		}
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if(value < 0) {
+			sr.flipX = true;
+		} else if(value > 0) {
+			sr.flipX = false;
+		}
+	}
+}
