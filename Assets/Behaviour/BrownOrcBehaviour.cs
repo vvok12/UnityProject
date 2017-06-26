ca911c1f82326eee408c4fde516d8986a6e02bbb
vvok12 <v.v.bezshtanko@gmail.com>
2017-06-26 19:37:59 +0300

    hw4

---
+++ Assets/Behaviour/BrownOrcBehaviour.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownOrcBehaviour : MonoBehaviour {
+	enum Target{
+		Rabbit, PointA, PointB, undef
+	}
+
+	public float radius=6.0f;
+	public Vector3 pointB;
+	public float speed=1.0f;
+	public float carrotDeltaTime = 2.0f;
+	public bool AlternativePositionMarker = false;
+	public Vector3 alternative_MoveDistVector;
+	Vector3 pointA;
+
+	private Animator myA;
+	private SpriteRenderer mySR;
+	private bool goingToA = false;
+	private bool needFlip = false;
+	void Start () {
+		reloadTime = carrotDeltaTime;
+
+		pointA = this.transform.position;
+		if (AlternativePositionMarker)
+			pointB = pointA + alternative_MoveDistVector;
+		myA = this.GetComponent<Animator> ();
+		mySR = this.GetComponent<SpriteRenderer> ();
+	}
+	// 0-idle, 1-walk, 2-run, 3-attack1, 4-attack2, 5-die
+	void SetAction(int recomended_animation){
+		myA.SetInteger ("action", recomended_animation);
+	}
+	int specialAction=0;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (specialAction != 0) {
+			SetAction (specialAction);
+			return;
+		}
+		SetAction (0);
+		Target targ = nextTarget ();
+
+		Vector3 old_pos = transform.position;
+
+		if (targ == Target.PointA) {
+			this.transform.position = Vector3.MoveTowards (old_pos, LevelController.zProjection (pointA, transform.position.z), speed*Time.deltaTime);
+			SetAction (1); 
+		} else if (targ == Target.PointB) {
+			this.transform.position = Vector3.MoveTowards (old_pos, LevelController.zProjection (pointB, transform.position.z), speed*Time.deltaTime);
+			SetAction (1);
+		} else if (targ == Target.Rabbit) {
+			Vector3 rabbi = HeroRabbit.lastRabbit.transform.position;
+			if (rabbi.x < transform.position.x)
+				mySR.flipX = false;
+			else
+				mySR.flipX = true;
+			SetAction (0);
+			tryThrow (rabbi.x - transform.position.x);
+			return;
+		}
+		old_pos -= transform.position;
+		if (old_pos.x > 0)
+			mySR.flipX = false;
+		else
+			mySR.flipX = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			if (Mathf.Abs (Mathf.Atan (
+				(transform.position.y - HeroRabbit.lastRabbit.transform.position.y) /
+				(transform.position.x - HeroRabbit.lastRabbit.transform.position.x)
+			)) < 1) {
+				
+			}else
+				die ();
+		}
+	}
+
+	Target nextTarget(){
+		if (Mathf.Abs(HeroRabbit.lastRabbit.transform.position.x - transform.position.x) <= this.radius)
+			return Target.Rabbit;
+
+		if (goingToA) {
+			if (isNear (transform.position, pointA))
+				goingToA = false;
+			return Target.PointA;
+		} else {
+			if (isNear (transform.position, pointB))
+				goingToA = true;
+			return Target.PointB;
+		}
+	}
+
+	bool isNear(Vector3 pos, Vector3 target)
+	{
+		pos.z = 0;
+		target.z = 0;
+		return Vector3.Distance (pos, target)<0.1f;
+	}
+
+	void die(){
+		Destroy (this.GetComponent<BoxCollider2D>());
+		Destroy (this.GetComponent<Rigidbody2D> ());
+		StartCoroutine (die_sub());
+	}
+	IEnumerator die_sub(){
+		specialAction = 4;
+		yield return new WaitForSeconds (3);
+		Destroy (this.gameObject);
+	}
+	IEnumerator attack(){
+		specialAction = 3;
+		yield return new WaitForSeconds (1);
+		specialAction = 0;
+	}
+	private float reloadTime;
+	void tryThrow(float xdir){
+		reloadTime += Time.deltaTime;
+		if (reloadTime > carrotDeltaTime) {
+			reloadTime = 0;
+			StartCoroutine (throwCarrot(xdir));
+		}
+	}
+	IEnumerator throwCarrot(float xdir){
+		specialAction = 3;
+		Weapon w = Weapon.newWeapon (xdir);
+		w.transform.position = transform.position + Vector3.up;
+		w.Go ();
+		yield return new WaitForSeconds (Mathf.Min(1.0f, carrotDeltaTime/2));
+		specialAction = 0;
+	}
+}
---
+++ Assets/Behaviour/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collectable {
+
+	public static GameObject proto;
+
+	public float speed = 1.0f;
+	public float lifetime = 3.0f;
+
+	private bool isActive = false;
+
+	void Start()
+	{
+		if (proto == null)
+			proto = GameObject.Find ("Carrot");
+	}
+
+	void FixedUpdate(){
+		if (!isActive)
+			return;
+		
+		Vector3 newPos = transform.position;
+		newPos.x += speedCoef * Time.deltaTime;
+		transform.position = newPos;
+	}
+
+	float speedCoef;
+
+	public static Weapon newWeapon(float xDirection)
+	{
+		Weapon newO = GameObject.Instantiate (proto).GetComponent<Weapon>();
+		if (xDirection < 0) {
+			newO.speedCoef = -1 * Weapon.proto.GetComponent<Weapon>().speed;
+			newO.GetComponent<SpriteRenderer> ().flipX = true;
+		} else {
+			newO.speedCoef = Weapon.proto.GetComponent<Weapon>().speed;
+		}
+		return newO;
+	}
+
+	public void Go(){
+		isActive = true;
+		StartCoroutine (DestroyerWait());
+	}
+
+	IEnumerator DestroyerWait(){
+		yield return new WaitForSeconds (lifetime);
+		Destroy (this.gameObject);
+	}
+	protected override void OnRabitHit(HeroRabbit rabit){
+		LevelController.current.OnRabbitDeath(rabit, false);
+		Destroy (this.gameObject);
+	}
+}
